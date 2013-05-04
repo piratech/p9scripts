4614a3d75ade4f2cd12eb953df8b2ddeb6c6ad23
Simon Marquardt <simon@localhost.localdomain>
2013-05-04 05:59:28 +0200

    Linux-Script hinzugefügt

---
+++ linux/drucker.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Fügt Drucker und Klassen als Remote-Drucker zu CUPS hinzu
+
+# lpadmin -p  -D "" -v ipp://drucker.berlin.pp/printers/ -E -L "P9a / "
+
+# DRUCKER
+lpadmin -p KARL-THEODOR -D "KARL-THEODOR (Xerox Workcentre Pro 245) [SW-Laser]" -v ipp://drucker.berlin.pp/printers/KARL-THEODOR -E -L "P9a / Küche"
+lpadmin -p SCHAVAN  -D "SCHAVAN (Brother HL-2250DN) [SW-Laser]" -v ipp://drucker.berlin.pp/printers/SCHAVAN -E -L "P9a / Büro"
+lpadmin -p VERONICA -D "VERONICA (Samsung CLP-510) [Farb-Laser]" -v ipp://drucker.berlin.pp/printers/VERONICA -E -L "P9a / Küche"
+#lpadmin -p SILVANA -D "SILVANA (HP Officejet 4500 G510g-m) [Farb-Tintenstrahler]" -v ipp://drucker.berlin.pp/printers/SILVANA -E -L "P9a / Büro"
+
+#KLASSEN
+padmin -p SW -D "Schwarzweiß-Drucker" -v ipp://drucker.berlin.pp/classes/SW -E -L "P9a"
+lpadmin -p COLOR -D "Farbdrucker" -v ipp://drucker.berlin.pp/classes/COLOR -E -L "P9a"
